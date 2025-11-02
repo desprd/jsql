4333dfd00d112ae79861901dd80ef0278082dc74
Ilya-Kanstanchuk <ilyakonst95@gmail.com>
2025-11-02 14:09:59 +0100

    Fix SelectQuery constructor to improve safetiness

--- src/main/java/com/ilyaproject/core/dto/query/SelectQuery.java
+++ src/main/java/com/ilyaproject/core/dto/query/SelectQuery.java
@@ -4,17 +4,13 @@ import com.ilyaproject.core.dto.expression.Expression;
 
 import java.util.ArrayList;
 import java.util.List;
-
 public record SelectQuery(
         List<String> tables,
         List<String> columns,
         Expression conditions
 ) implements SQLQuery {
-    public SelectQuery() {
-        this(
-                new ArrayList<>(),
-                new ArrayList<>(),
-                null
-        );
+    public SelectQuery {
+        tables = tables == null ? new ArrayList<>() : new ArrayList<>(tables);
+        columns = columns == null ? new ArrayList<>() : new ArrayList<>(columns);
     }
 }
